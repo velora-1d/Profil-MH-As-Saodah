838b0ae4e4b2d827d1745fe8ffa46cbae76b2ccf
Mahin Utsman Nawawi, S.H <mahinutsmannawawi20@gmail.com>
2026-04-24 22:43:06 +0700

    fix: correct class name typo in Dialog component

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -45,7 +45,7 @@ const DialogContent = React.forwardRef<
     >
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 data-[state=open]:text-slate-500">
-        <X className="h-4 h-4" />
+        <X className="w-4 h-4" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
